7aff84288b6ff1996c3bbcc52d05eb13e4531252
Rodion <leon172071@gmail.com>
2022-11-15 15:55:10 +0300

    CodeLabs 2, 4

---
+++ Android task 2/app/src/main/java/com/example/androidtask2/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.androidtask2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.rollButton)
+        rollButton.setOnClickListener { rollDice()}
+    }
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceRoll2 = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+        // Update the screen with the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage2.setImageResource(drawableResource2)
+
+        diceImage.contentDescription = diceRoll.toString()
+        diceImage2.contentDescription = diceRoll2.toString()
+        val sum = diceRoll + diceRoll2
+        Toast.makeText(this, "Sum of Roll $sum  ", Toast.LENGTH_SHORT).show()
+    }
+}
+
+class Dice(private val numSide: Int) {
+    fun roll(): Int {
+        return (1..numSide).random()
+    }
+}
