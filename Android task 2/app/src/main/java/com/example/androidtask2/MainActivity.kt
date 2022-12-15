15a3d008f00672cef23cb61a71d5d9c39c2514b3
Rodion <leon172071@gmail.com>
2022-12-15 14:04:45 +0300

    CodeLab Hilt

--- Android task 2/app/src/main/java/com/example/androidtask2/MainActivity.kt
+++ Android task 2/app/src/main/java/com/example/androidtask2/MainActivity.kt
@@ -16,15 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.rollButton)
-        rollButton.setOnClickListener { rollDice()
-        var count: Int = 0
-        while(count < 100){
-            count ++
-            Toast.makeText(this, "Count of toast $count  ", Toast.LENGTH_SHORT).show()
-        }
-        }
-
-
+        rollButton.setOnClickListener { rollDice() }
 
     }
     /**
